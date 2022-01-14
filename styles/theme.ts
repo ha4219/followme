fbfbda391de8f463a3c9b1a8ad4a533a31d96006
ha4219 <ha4219@naver.com>
2022-01-14 22:30:27 +0900

    [ha4219] add global font

---
+++ styles/theme.ts
@@ -0,0 +1,28 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { deepPurple, amber } from "@mui/material/colors";
+
+// Create a theme instance.
+let theme = createTheme({
+  // palette: {
+  //   primary: deepPurple,
+  //   secondary: amber,
+  // },
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
