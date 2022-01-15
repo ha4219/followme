12314e2ef2e66e7476241150cecd5805d14eec23
ha4219 <ha4219@naver.com>
2022-01-16 02:38:07 +0900

    [ha4219] add banner

--- components/Banner/styles.ts
+++ components/Banner/styles.ts
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 export const BannerContainer = styled.div`
   display: flex;
   margin: 0;
+  --swiper-theme-color: white;
+
   & .swiper-slide {
     height: 30rem;
     width: 10rem;
