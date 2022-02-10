ba988ea4634cde285c984015ce1d41b722d6529b
ha4219 <ha4219@naver.com>
2022-02-11 00:28:53 +0900

    [ha4219] edit tag container

--- components/TagContainer/styles.ts
+++ components/TagContainer/styles.ts
@@ -5,11 +5,21 @@ export const TagConv = styled(Box)`
   padding: 1rem 0;
   display: flex;
 
+  color: #b69775;
+  font-weight: bold;
+
   & span {
-    border: 1px solid #3e3e3e;
+    border: 1px solid #b69775;
     border-radius: 1rem;
+
     padding: 0.3rem;
     font-size: 0.8rem;
-    color: #3e3e3e;
+    text-align: center;
+    vertical-align: center;
+  }
+
+  & span:hover {
+    background-color: #b69775;
+    color: #ffffff;
   }
 `;
