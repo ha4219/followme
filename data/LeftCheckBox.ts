f00794d87eedcd7dd5e22b2af2b5d0e84a521088
ha4219 <ha4219@naver.com>
2022-05-15 23:31:43 +0900

    ADD 지역별 태그

--- data/LeftCheckBox.ts
+++ data/LeftCheckBox.ts
@@ -15,6 +15,13 @@ export const DATAL = <IData>{
     { name: "서울", value: "서울" },
     { name: "경기도", value: "경기도" },
     { name: "강원도", value: "강원도" },
+    { name: "경북", value: "경북" },
+    { name: "경남", value: "경남" },
+    { name: "전남", value: "전남" },
+    { name: "전북", value: "전북" },
+    { name: "충북", value: "충북" },
+    { name: "충남", value: "충남" },
+    { name: "제주", value: "제주" },
   ],
   overseas: [
     { name: "일본", value: "일본" },
