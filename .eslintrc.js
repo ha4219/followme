cc44c3304e36b769e0ffded0cfb4b04f698f0426
ha4219 <ha4219@naver.com>
2022-06-21 22:05:51 +0900

    tmp

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "prettier", "import"],
   rules: {
     "prefer-const": "error",
   },
